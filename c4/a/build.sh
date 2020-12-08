66bd5e98c7d1a66c597aff3fd40d77ab99eee7f5
huang1125677925 <1125677925@qq.com>
2020-12-08 10:48:26 +0800

    os lab

---
+++ c4/a/build.sh
@@ -0,0 +1,9 @@
+bximage -hd=10M -mode=create -q disk.img
+
+nasm -I boot/include/ -o mbr.bin boot/mbr.S 
+nasm -I boot/include/ -o loader.bin boot/loader.S 
+dd if=./mbr.bin of=disk.img bs=512 count=1 conv=notrunc
+dd if=./loader.bin of=disk.img bs=512 count=4 seek=2 conv=notrunc
+
+
+
